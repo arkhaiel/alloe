5ebe5170fd0bd8556169c32b73afdd8957a6f346
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-10-21 18:48:59 +0200

    update

--- components/header.vue
+++ components/header.vue
@@ -25,13 +25,7 @@ const items = [
   <div class="header sticky top-0 z-50 w-full backdrop-blur grid grid-cols-3 mb-8 border-b border-gray-900/10 dark:border-gray-50/[0.06] bg-white/75 dark:bg-gray-900/75">
   <div class="flex flex-row justify-start content-center items-center gap-2 pr-2">
   <Logo :isDark="isDark" />
-  <UButton
-      :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
-      color="gray"
-      variant="ghost"
-      aria-label="Theme"
-      @click="isDark = !isDark"
-    />
+  
   </div>
   <div class="flex flex-row justify-center items-center">
     <UButton label="news" variant="ghost" to="/news" />
@@ -39,6 +33,13 @@ const items = [
 
   <div class="flex flex-row justify-end content-center items-center gap-2 pr-2">
     <ColorPicker />
+    <UButton
+      :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+      color="gray"
+      variant="ghost"
+      aria-label="Theme"
+      @click="isDark = !isDark"
+    />
     <UDropdown :items="items" v-if="user && userData">
       <UButton color="white" :label="userData.username ? userData.username : 'incomplet'" />
     </UDropdown>
