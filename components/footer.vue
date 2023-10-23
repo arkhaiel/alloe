cb432c0df77a69efff6db3f252a9c81e3f1f4a0c
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-10-23 21:52:30 +0200

    update

--- components/footer.vue
+++ components/footer.vue
@@ -1,17 +1,21 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <div class="header w-full mt-4 flex flex-row items-center justify-around  bg-white/75 dark:bg-gray-900/75">
+  <div class="header w-full mt-4 flex flex-row items-center justify-around">
     <div class="flex flex-row gap-4">
-    <UButton variant="ghost" size="2xs" color="gray" label="Site réalisé par M. NICOLAS" to="mailto:mathieu@alloe.fr" />
-    <UButton variant="ghost" size="2xs" color="gray" label="CGU" to="/cgu" />
+      <UButton
+        variant="ghost"
+        size="2xs"
+        color="gray"
+        label="Site réalisé par M. NICOLAS"
+        to="mailto:mathieu@alloe.fr"
+      />
+      <UButton variant="ghost" size="2xs" color="gray" label="CGU" to="/cgu" />
     </div>
   </div>
 </template>
 
 <style scoped>
-
 .titre {
   font-size: 2rem;
   flex-grow: 1;
