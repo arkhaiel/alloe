fc37f5c7e3d788308b4461756b41b51d420981b4
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-10-24 23:14:46 +0200

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,7 +64,7 @@ export default defineNuxtConfig({
       redirectOptions: {
         login: '/login',
         callback: '/confirm',
-        exclude: ['/', '/news/*', '/news', '/cgu', '/cgu/*', '/lire', '/lire/*']
+        exclude: ['/', '/news/*', '/news', '/cgu', '/cgu/*']
       }
     }
   })
