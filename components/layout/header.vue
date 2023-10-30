f925233b7c5a762f4c63917dfaa5809c56e315bc
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-10-30 20:38:17 +0100

    modification header pour petits écrans

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -38,31 +38,31 @@ const tutoToggle = async() => {
 
 <template>
   <div
-    class="sticky top-0 z-50 w-full backdrop-blur-sm grid grid-cols-1 sm:grid-cols-2 mb-8"
+    class="sticky top-0 z-50 w-full backdrop-blur-sm flex flex-row flex-wrap justify-between items-center mb-8"
   >
-    <div class="flex flex-row justify-start content-center items-center gap-2 pr-2">
+    <div class="">
       <Logo :isDark="isDark" />
+    </div>
+    <div class="order-3 sm:order-2 min-w-full sm:min-w-0">
       <UButton label="lire" variant="soft" to="/lire" v-if="us.user" />
       <UButton label="mes textes" variant="soft" to="/mes-textes" v-if="us.user" />
     </div>
-
-
     <div
-      class="flex flex-row justify-end content-center items-center gap-2 pr-2"
+      class="flex flex-row order-2 sm:order-3 justify-end content-center items-center gap-2 pr-2"
     >
-    <UButton icon="i-heroicons-lifebuoy" label="tuto" color="primary" @click="tutoToggle" variant="soft" v-if="us.user" />
     <UButton icon="i-heroicons-newspaper" label="news" color="primary" to="/news" variant="soft" />
+    <UButton icon="i-heroicons-lifebuoy" color="primary" @click="tutoToggle" variant="soft" v-if="us.user" />
       <UButton
         :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
         color="primary"
-        variant="outline"
+        variant="ghost"
         aria-label="Theme"
         @click="isDark = !isDark"
       />
       <UDropdown :items="items" v-if="us.user && us.userData">
         <UButton
           color="primary"
-          :label="us.userData.username ? us.userData.username : 'incomplet'"
+          icon="i-heroicons-user"
         />
       </UDropdown>
       <UButton v-else color="gray" label="Connexion" to="/login" />
