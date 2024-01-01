4a8ab18e05cdc85416e91818249df25587ccebe5
Mathieu NICOLAS <mathieu.nicolas@me.com>
2024-01-02 00:04:08 +0100

    disclaimer écriture suite

--- components/suiteMenu.vue
+++ components/suiteMenu.vue
@@ -8,35 +8,48 @@
   />
   <UTabs
     orientation="vertical"
-    v-if="items "
+    v-if="items"
     :items="items"
     v-model="selected"
     :key="enfants[0]"
     :ui="{
       wrapper: 'flex items-center gap-0 md:gap-4 h-[12rem] space-y-0',
-      list: { width: 'w-48',  },
+      list: { width: 'w-48' },
       container: 'h-full m-0 px-4 overflow-y-scroll self-start',
-      base: 'h-full'
+      base: 'h-full',
     }"
   >
     <template #item="{ item }">
-    <div class="flex flex-row justify-end">
-      <UButton variant="ghost" :to="'/ecrire/'+item.id" size="xs" v-if="item.author === us.user.id">modifier mon chapitre</UButton>
-    </div>
+      <div class="flex flex-row justify-end">
+        <UButton
+          variant="ghost"
+          :to="'/ecrire/' + item.id"
+          size="xs"
+          v-if="item.author === us.user.id"
+          >modifier mon chapitre</UButton
+        >
+      </div>
       <div
         class="prose static dark:prose-invert whitespace-pre-wrap pt-0 prose-p:first:m-0 text-[0.92rem]/6 px-0 sm:px-4 text-justify prose-p:indent-4 max-w-none grow"
-      ><MdComp>{{ item.content }}</MdComp>
+      >
+        <MdComp>{{ item.content }}</MdComp>
       </div>
     </template>
 
     <template #vide="{ item }">
-      <div class="flex h-full w-full flex-row justify-center items-center">
+      <div class="flex h-full w-full flex-col justify-center items-center">
         <UButton
           size="xl"
           label="Écrire une suite"
           variant="outline"
           @click="newChap(read.storyList[read.storyList.length - 1])"
         />
+        <p class="mt-2">
+          Attention: en cliquant sur ce bouton, vous allez automatiquement créer un
+          nouveau chapitre. Vous ne pourrez pas le supprimer, l'emplacement sera réservé
+          pour vous. Vous pourrez cependant commencer son écriture et réaliser les
+          modifications que vous souhaitez plus tard.
+        </p>
       </div>
     </template>
   </UTabs>
@@ -53,24 +66,28 @@
 
 <script lang="ts" setup>
 const read = useReadingStore();
-const us = useCounterStore()
+const us = useCounterStore();
 const selected = ref(0);
 const parid = useRoute().params.id;
-const working = ref(false)
+const working = ref(false);
 const items = ref();
 const { storyList, enfants } = storeToRefs(read);
-
 
 watch(enfants, () => {
   items.value = [];
   items.value = makeItems();
-  selected.value = 0
+  selected.value = 0;
 });
 
 const makeItems = () => {
   items.value = enfants.value
     ? enfants.value.map((el: any, idx: number) => {
-        return { label: `suite ${idx + 1} ${el.author === us.user.id ? '*' : ''}`, content: el.text, id: el.id, author: el.author };
+        return {
+          label: `suite ${idx + 1} ${el.author === us.user.id ? "*" : ""}`,
+          content: el.text,
+          id: el.id,
+          author: el.author,
+        };
       })
     : [];
   while (items.value.length < read.current?.root.constrains.max_choices)
@@ -86,9 +103,13 @@ const newChap = async (id_parent: string) => {
 };
 
 const addSuite = async (item: any) => {
-  working.value = true
-  if(read.story === undefined || read.story.length === 0) read.story = [read.current.root];
-  await nextChap(read.enfants.find(el => el.id === item.id), 'blabla')
+  working.value = true;
+  if (read.story === undefined || read.story.length === 0)
+    read.story = [read.current.root];
+  await nextChap(
+    read.enfants.find((el) => el.id === item.id),
+    "blabla"
+  );
   // if (useRoute().path === "/lire") {
   //   read.reading = await read.newReading(item.id, read.current.root);
 
@@ -107,7 +128,7 @@ const addSuite = async (item: any) => {
   //     true
   //   );
   // }
-  working.value = false
+  working.value = false;
 };
 </script>
 
